a55f863783f0fa8928eeae6151276ea28e252c10
Silke Blansjaar <silkeblansjaar@gmail.com>
2021-02-13 09:55:53 +0000

    Added ability to edit text

--- src/script.js
+++ src/script.js
@@ -4,6 +4,7 @@ let newTask = textInput.value
 let toDoListElement = document.querySelector("#to-do-list");
 let deleteButtons = document.querySelectorAll(".delete-button");
 let checkboxes = document.querySelectorAll(".form-check-input");
+let editButtons = document.querySelectorAll(".pencil");
 
 function createNewListElement(newTask) {
   let listItem = document.createElement("li");
@@ -15,7 +16,7 @@ function createNewListElement(newTask) {
   listItem.className = "list-item row";
   checkBox.type = "checkbox";
   checkBox.className = "form-check-input col-1";
-  taskItem.className = "col-9";
+  taskItem.className = "list-text col-9";
   taskItem.innerText = newTask;
   editButton.className = "pencil col-1 fas fa-pencil-alt";
   deleteButton.className = "delete-button far fa-times-circle col-1";
@@ -42,6 +43,38 @@ function checkTask() {
   }
 }
 
+function editTask() {
+  let listItem = this.parentNode;
+  let task = listItem.querySelector(".list-text");
+  
+  if (listItem.matches(".edit")) {
+    let editedTask = listItem.querySelector(".edit-window");
+    let newTask = document.createElement("label");
+    newTask.className = "list-text col-9"
+    newTask.innerHTML = editedTask.value;
+    
+    listItem.replaceChild(newTask, editedTask);
+    
+    this.classList.remove("fa-check");
+    this.classList.add("fa-pencil-alt");
+    
+    listItem.classList.remove("edit")
+  } else {
+    let editWindow = document.createElement("input");
+    let oldTask = listItem.querySelector("label").innerHTML;
+    editWindow.type = "text";
+    editWindow.className = "edit-window col-9";
+    editWindow.value = oldTask;
+    
+    listItem.replaceChild(editWindow, task);
+    
+    this.classList.remove("fa-pencil-alt");
+    this.classList.add("fa-check");
+    
+    listItem.classList.add("edit");
+  }
+}
+
 function setNewTask(event) {
   newTask = event.target.value;
 }
@@ -59,6 +92,8 @@ function handleSubmit(event) {
     deleteButtons.forEach(item => item.addEventListener("click", deleteTask));
     checkboxes = document.querySelectorAll(".form-check-input");
     checkboxes.forEach(item => item.addEventListener("change", checkTask));
+    editButtons = document.querySelectorAll(".pencil");
+    editButtons.forEach(item => item.addEventListener("click", editTask));
   }
 }
 
@@ -66,3 +101,4 @@ textInput.addEventListener("change", setNewTask);
 inputForm.addEventListener("submit", handleSubmit);
 deleteButtons.forEach(item => item.addEventListener("click", deleteTask));
 checkboxes.forEach(item => item.addEventListener("change", checkTask));
+editButtons.forEach(item => item.addEventListener("click", editTask));
